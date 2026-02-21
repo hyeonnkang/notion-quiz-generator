6db64caee6eaf03beebf311db3ec921024ef2dd1
강현 <khyeon.dev@gmail.com>
2026-02-21 23:54:50 +0900

    refactor: NotionRequest에서 불필요한 @Service 어노테이션 제거

--- server/src/main/java/com/notionquiz/generator/dto/NotionRequest.java
+++ server/src/main/java/com/notionquiz/generator/dto/NotionRequest.java
@@ -1,10 +1,8 @@
 package com.notionquiz.generator.dto;
 
 import lombok.Getter;
-import org.springframework.stereotype.Service;
 
 @Getter
-@Service
 public class NotionRequest {
     private String pageId;
 }
